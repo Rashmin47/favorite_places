34e4ed16021cb0999c5c07b8420a7dcc835386e2
rashmin <rashminxperience@gmail.com>
2026-01-18 17:26:22 +0545

    Added local save functionality

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,11 +2,16 @@ import 'dart:io';
 
 import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>>{
   UserPlacesNotifier() : super(const []);
-  void addPlace(String title, File image) {
-    final newPlace = Place(title: title, image: image);
+  void addPlace(String title, File image) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+    final newPlace = Place(title: title, image: copiedImage);
     state = [newPlace,...state];
   }
 }
